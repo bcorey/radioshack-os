eb42ec5e4fe15e363acb51cd38b1e80761b6d3b5
bcorey <benjaminmcorey@gmail.com>
2025-12-29 18:07:53 -0500

    clean up window settings

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,13 @@ fn main() {
         .add_plugins((
             DefaultPlugins.set(WindowPlugin {
                 primary_window: Some(Window {
-                    title: "I am a window!".into(),
-                    name: Some("bevy.app".into()),
+                    title: "RadioshackOS".into(),
                     resolution: (800, 600).into(),
                     present_mode: PresentMode::AutoVsync,
                     // Tells Wasm to resize the window according to the available canvas
                     fit_canvas_to_parent: false,
                     // Tells Wasm not to override default event handling, like F5, Ctrl+R etc.
                     prevent_default_event_handling: false,
-                    window_theme: None,
-                    enabled_buttons: bevy::window::EnabledButtons {
-                        maximize: false,
-                        ..Default::default()
-                    },
                     // This will spawn an invisible window
                     // The window will be made visible in the make_visible() system after 3 frames.
                     // This is useful when you want to avoid the white window that shows up before the GPU is ready to render the app.
